183bacf4b6632c203090b25e94b39f24d62b6db1
Recep Borekci <recep.borekci@qaracter.com>
2026-01-14 15:39:18 +0300

    feat: Add getters and setters in Entities

--- src/main/java/com/example/finalexam/model/AccountEntity.java
+++ src/main/java/com/example/finalexam/model/AccountEntity.java
@@ -18,4 +18,44 @@ public class AccountEntity {
     private Double balance;
     @OneToMany(mappedBy = "account", cascade = CascadeType.ALL, orphanRemoval = true)
     private List<TransactionEntity> transactions;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getOwnerName() {
+        return ownerName;
+    }
+
+    public void setOwnerName(String ownerName) {
+        this.ownerName = ownerName;
+    }
+
+    public String getIban() {
+        return iban;
+    }
+
+    public void setIban(String iban) {
+        this.iban = iban;
+    }
+
+    public Double getBalance() {
+        return balance;
+    }
+
+    public void setBalance(Double balance) {
+        this.balance = balance;
+    }
+
+    public List<TransactionEntity> getTransactions() {
+        return transactions;
+    }
+
+    public void setTransactions(List<TransactionEntity> transactions) {
+        this.transactions = transactions;
+    }
 }
--- src/main/java/com/example/finalexam/model/TransactionEntity.java
+++ src/main/java/com/example/finalexam/model/TransactionEntity.java
@@ -25,5 +25,51 @@ public class TransactionEntity {
     @JoinColumn(name = "account_id")
     private AccountEntity account;
 
+    public Long getId() {
+        return id;
+    }
 
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public TransactionType getType() {
+        return type;
+    }
+
+    public void setType(TransactionType type) {
+        this.type = type;
+    }
+
+    public Double getAmount() {
+        return amount;
+    }
+
+    public void setAmount(Double amount) {
+        this.amount = amount;
+    }
+
+    public LocalDateTime getDate() {
+        return date;
+    }
+
+    public void setDate(LocalDateTime date) {
+        this.date = date;
+    }
+
+    public Boolean getFlagged() {
+        return isFlagged;
+    }
+
+    public void setFlagged(Boolean flagged) {
+        isFlagged = flagged;
+    }
+
+    public AccountEntity getAccount() {
+        return account;
+    }
+
+    public void setAccount(AccountEntity account) {
+        this.account = account;
+    }
 }
