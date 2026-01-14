0ff92fa20168385111d48b75371654332576d8de
Recep Borekci <recep.borekci@qaracter.com>
2026-01-14 15:06:54 +0300

    feat: Created AccountRepository and TransactionRepository

---
+++ src/main/java/com/example/finalexam/repository/AccountRepository.java
@@ -0,0 +1,10 @@
+package com.example.finalexam.repository;
+
+import com.example.finalexam.model.AccountEntity;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface AccountRepository extends JpaRepository<AccountEntity, Long> {
+
+}
